6295777340adf92b8e78b1acaf8bacdb91440a67
Aniket Vashishtha <t-aniketva@microsoft.com>
2022-12-28 14:32:19 +0530

    Push Notifs

---
+++ android/app/src/main/kotlin/com/microsoft/zen_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.microsoft.zen_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[],
+    )));
+  }
+}
